aca21a9879e99b0feab0c520a9312be32a0a335b
xuwei_k <6b656e6a69@gmail.com>
2011-09-25 21:16:16 +0900

    bug fix

--- src/main/scala/xuwei_k/classDiagram/DiagramService.scala
+++ src/main/scala/xuwei_k/classDiagram/DiagramService.scala
@@ -13,29 +13,27 @@ object DiagramService {
     (x * (y % 2 + 1)) + y % 3
   }
 
-  /**
-   * 実際のインスタンスから作成
-   * @param fileName 保存するファイル名
+  /** 実際のインスタンスから作成
+   * @param name 
    * @param objList 作成元のオブジェクト
    */
-  def createClassDiagramByObj(fileName: String)(objList: AnyRef*): List[Node] = {
-    create(fileName, objList.map { _.getClass })
+  def createClassDiagramByObj(name: String)(objList: AnyRef*): List[Node] = {
+    create(name, objList.map { _.getClass })
   }
 
-  /**
-   * クラスの完全修飾名から作成
-   * @param fileName 保存するファイル名
+  /** クラスの完全修飾名から作成
+   * @param name
    * @param classNames 作成するクラス名
    */
-  def createClassDiagramByName(fileName: String)(classNames: String*): List[Node] = {
-    create(fileName, classNames.map { Class.forName(_) })
+  def createClassDiagramByName(name: String)(classNames: String*): List[Node] = {
+    create(name, classNames.map { Class.forName(_) })
   }
 
-  def createClassDiagramByClass(fileName: String)(classes: Class[_]*): List[Node] = {
-    create(fileName, classes)
+  def createClassDiagramByClass(name: String)(classes: Class[_]*): List[Node] = {
+    create(name, classes)
   }
 
-  private def create(fileName: String, classes: Traversable[Class[_]]): List[Node] = {
+  private def create(name: String, classes: Traversable[Class[_]]): List[Node] = {
 
     import collection.{ mutable => mu }
 
@@ -46,11 +44,11 @@ object DiagramService {
       }.distinct.filter { c => !ClassNode.exceptList.contains(c.clazz) })
 
     //デバック用表示
-    result.foreach {
+/*  result.foreach {
       case (n, list) =>
         println(n, list.size, list.map { _.clazz.getSimpleName })
     }
-
+*/
     //横の位置計算して決めて、ClassNodeオブジェクトのフィールドに保存
     ClassNode.allClassNodes =
       result.flatMap {
@@ -62,21 +60,14 @@ object DiagramService {
 
     //xmlに変換
     ClassNode.allClassNodes.filterNot { x => ClassNode.exceptList.contains(x) }.map { _.toXml }
-
   }
 
-  /**
-   * (ソートしてない)ClassNodeのList作成
+  /** (ソートしてない)ClassNodeのList作成
    */
   private def makeClassNodes(clazz: Class[_]): List[ClassNode] = {
     getAllClassAndTrait(clazz).map { x =>
-      val superClass = x.getSuperclass
-      val classes =
-        if (superClass != null) {
-          superClass :: x.getInterfaces.toList
-        } else {
-          x.getInterfaces.toList
-        }
+      val interfaces = x.getInterfaces.toList
+      val classes = Option(x.getSuperclass).map{_ :: interfaces }.getOrElse(interfaces)
       ClassNode(x, 0, classes: _*)
     }
   }
@@ -87,16 +78,16 @@ object DiagramService {
    */
   private def sortByInheritance(classList: List[ClassNode]): List[Pair[Int, List[ClassNode]]] = {
 
-    /** ローカルのヘルパー関数,再帰 */
-    @scala.annotation.tailrec
+    @annotation.tailrec
     def loop(m: List[ClassNode]) {
-      val oldMax = m.foldLeft(0) { (x, y) => x max y.level }
-      for (x <- m; y <- m) { //総当りで呼ぶ
+      def levelMap():Map[Int,Int] = m.map{_.level}.groupBy(identity).mapValues{_.size}
+
+      val oldMap = levelMap()
+      for (x <- m; y <- m){ 
         x.compare(y)
       }
-      println("loop") //debug
-      val newMax = m.foldLeft(0) { (x, y) => x max y.level }
-      if (oldMax != newMax) {
+
+      if( oldMap != levelMap() ){ 
         loop(m)
       }
     }
