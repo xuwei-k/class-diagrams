5d68886661ce9abba9461ecc853476cd1e8f33cf
xuwei-k <6b656e6a69@gmail.com>
2012-12-07 12:42:54 +0900

    sbt-updates plugin

--- project/plugins.sbt
+++ project/plugins.sbt
@@ -3,3 +3,5 @@ resolvers ++= Seq(
   Resolver.url("sbt-plugin-releases",
     url("http://scalasbt.artifactoryonline.com/scalasbt/sbt-plugin-releases/"))(Resolver.ivyStylePatterns)
 )
+
+addSbtPlugin("com.timushev.sbt" % "sbt-updates" % "0.1.0")
--- project/project/plugins.scala
+++ project/project/plugins.scala
@@ -3,7 +3,6 @@ import sbt._
 object PluginDef extends Build {
   lazy val root = Project("plugins", file(".")) dependsOn(
     uri("git://github.com/sbt/sbt-appengine#d2778bd4c3acc8495555a964a92404fb67264fe1")
-   ,uri("git://github.com/rtimush/sbt-updates#04aa05573214fd3c5c57d")
   )
 }
 
