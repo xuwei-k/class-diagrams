cd3b173bae5ba598dfb5006059646e43ab952995
xuwei-k <6b656e6a69@gmail.com>
2014-04-19 02:41:26 +0900

    update

--- project/ClassDiagramsBuild.scala
+++ project/ClassDiagramsBuild.scala
@@ -6,13 +6,13 @@ object ClassDiagramBuild extends Build{
   val root = Project("class-diagrams", file(".")).settings(
     sbtappengine.Plugin.appengineSettings: _*
   ).settings(
-    scalaVersion := "2.11.0-RC3",
+    scalaVersion := "2.11.0",
     scalacOptions ++= Seq("-deprecation", "-Xlint", "-unchecked", "-language:_"),
     libraryDependencies ++= Seq(
        "org.eclipse.jetty" % "jetty-webapp" % "7.6.8.v20121106" % "container"
       ,"javax.servlet" % "servlet-api" % "2.5"
-      ,"org.specs2" %% "specs2" % "2.3.10"
-      ,"com.typesafe.akka" %% "akka-actor" % "2.3.0"
+      ,"org.specs2" %% "specs2" % "2.3.11"
+      ,"com.typesafe.akka" %% "akka-actor" % "2.3.2"
       ,"org.scala-lang" % "scala-compiler" % scalaVersion.value
       ,"org.scala-lang" % "scalap" % scalaVersion.value
       ,"org.scalaz" %% "scalaz-scalacheck-binding" % "7.0.6"
@@ -22,6 +22,6 @@ object ClassDiagramBuild extends Build{
       ExclusionRule(organization = "javax.jms")
     ))
   )
-  
+
 }
 
