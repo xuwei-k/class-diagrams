c033881346b74a434d0440a985a2a58d7d976da1
xuwei-k <6b656e6a69@gmail.com>
2014-07-04 10:28:56 +0900

    update

--- project/ClassDiagramsBuild.scala
+++ project/ClassDiagramsBuild.scala
@@ -12,15 +12,15 @@ object ClassDiagramBuild extends Build{
       ("org.eclipse.jetty" % "jetty-webapp" % "7.6.15.v20140411" % "container") ::
       ("com.chuusai" %% "shapeless" % "2.0.0") ::
       ("org.scala-lang.modules" %% "scala-async" % "0.9.1") ::
-      ("org.scalikejdbc" %% "scalikejdbc" % "2.0.0") ::
+      ("org.scalikejdbc" %% "scalikejdbc" % "2.0.4") ::
       ("com.github.scopt" %% "scopt" % "3.2.0") ::
       ("javax.servlet" % "servlet-api" % "2.5") ::
-      ("org.scalatest" %% "scalatest" % "2.1.7") ::
-      ("org.specs2" %% "specs2" % "2.3.12") ::
-      ("com.typesafe.akka" %% "akka-actor" % "2.3.3") ::
+      ("org.scalatest" %% "scalatest" % "2.2.0") ::
+//      ("org.specs2" %% "specs2" % "2.3.12") ::
+      ("com.typesafe.akka" %% "akka-actor" % "2.3.4") ::
       ("org.scala-lang" % "scala-compiler" % scalaVersion.value) ::
       ("org.scala-lang" % "scalap" % scalaVersion.value) ::
-      ("org.scalaz" %% "scalaz-scalacheck-binding" % "7.1.0-M7") ::
+      ("org.scalaz" %% "scalaz-scalacheck-binding" % "7.1.0-RC1") ::
       ("org.json4s" %% "json4s-native" % "3.2.10") ::
       ("net.debasishg" %% "redisclient" % "2.13") ::
       Nil
