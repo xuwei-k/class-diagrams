a39d62a4aa88b15880acf1e1ae40d0db9ff2033c
xuwei-k <6b656e6a69@gmail.com>
2014-07-25 03:11:09 +0900

    update

--- project/ClassDiagramsBuild.scala
+++ project/ClassDiagramsBuild.scala
@@ -6,13 +6,13 @@ object ClassDiagramBuild extends Build{
   val root = Project("class-diagrams", file(".")).settings(
     sbtappengine.Plugin.appengineSettings: _*
   ).settings(
-    scalaVersion := "2.11.1",
+    scalaVersion := "2.11.2",
     scalacOptions ++= Seq("-deprecation", "-Xlint", "-unchecked", "-language:_", "-Ywarn-unused", "-Ywarn-unused-import"),
     libraryDependencies ++= (
       ("org.eclipse.jetty" % "jetty-webapp" % "7.6.15.v20140411" % "container") ::
       ("com.chuusai" %% "shapeless" % "2.0.0") ::
-      ("org.scala-lang.modules" %% "scala-async" % "0.9.1") ::
-      ("org.scalikejdbc" %% "scalikejdbc" % "2.0.4") ::
+      ("org.scala-lang.modules" %% "scala-async" % "0.9.2") ::
+      ("org.scalikejdbc" %% "scalikejdbc" % "2.0.6") ::
       ("com.github.scopt" %% "scopt" % "3.2.0") ::
       ("javax.servlet" % "servlet-api" % "2.5") ::
       ("org.scalatest" %% "scalatest" % "2.2.0") ::
